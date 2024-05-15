1a451933f1942bf831a027c6bb80ac9597231e16
ehgkals <ehgkals12@gmail.com>
2024-05-16 00:50:08 +0900

    Retrofit2 php 통신(HeatShelter) - main에서 분리

---
+++ app/src/main/java/com/example/simpyo/api/HeatShelterList.kt
@@ -0,0 +1,31 @@
+package com.example.simpyo.api
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+import com.example.simpyo.dataclasses.HeatShelterData
+import com.example.simpyo.interfaces.HeatShelterInterface
+
+class HeatShelterList {
+    fun getHeatShelterList(): List<HeatShelterData>? {
+        val retrofit = RetrofitClientObject.getInstance()
+        val service = retrofit.create(HeatShelterInterface::class.java)
+        var heatShelterList : List<HeatShelterData>? = emptyList()
+
+        service.requestHeatShelterData().enqueue(object : Callback<List<HeatShelterData>> {
+            override fun onResponse(
+                call: Call<List<HeatShelterData>>,
+                response: Response<List<HeatShelterData>>
+            ) {
+                heatShelterList = response.body()
+            }
+
+            override fun onFailure(call: Call<List<HeatShelterData>>, t: Throwable) {
+
+            }
+        })
+
+        return heatShelterList
+    }
+}
