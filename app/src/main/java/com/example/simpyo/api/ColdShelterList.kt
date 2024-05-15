8410026cd9d62c6046934d1d3af3886db5944619
jeenkeem <jinijinikim@naver.com>
2024-05-16 01:29:58 +0900

    Retrofit2 php 통신(HeatShelter + ColdShelter)

--- app/src/main/java/com/example/simpyo/api/ColdShelterList.kt
+++ app/src/main/java/com/example/simpyo/api/ColdShelterList.kt
@@ -1,4 +1,31 @@
 package com.example.simpyo.api
 
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+import com.example.simpyo.dataclasses.ColdShelterData
+import com.example.simpyo.interfaces.ColdShelterInterface
+
 class ColdShelterList {
+    fun getColdShelterList(): List<ColdShelterData>? {
+        val retrofit = RetrofitClientObject.getInstance()
+        val service = retrofit.create(ColdShelterInterface::class.java)
+        var coldShelterList : List<ColdShelterData>? = emptyList()
+
+        service.requestColdShelterData().enqueue(object : Callback<List<ColdShelterData>> {
+            override fun onResponse(
+                call: Call<List<ColdShelterData>>,
+                response: Response<List<ColdShelterData>>
+            ) {
+                coldShelterList = response.body()
+            }
+
+            override fun onFailure(call: Call<List<ColdShelterData>>, t: Throwable) {
+
+            }
+        })
+
+        return coldShelterList
+    }
 }
